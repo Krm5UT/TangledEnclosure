0f4562aa01bd30fa46b25e694b3f9d2e0e8147cd
Krm5UT <krm4672@my.utexas.edu>
2025-11-17 15:22:52 -0600

    Update TangledCode.ino

--- TangledCode.ino
+++ TangledCode.ino
@@ -1,53 +1,59 @@
 #include <Servo.h>
 
 Servo flowerServo;  // create Servo object to control a servo
-const int flowerButton;
-const int moonButton; 
+int flowerButt;
+const int flowerButtPin = 3;
+int flowerButtState = 0;
+int lastflowerButtState = 0;
+int flowerButtVal;
+bool flower = false;
+
+const int moonButton;
 Servo hairServo;
 const int flynnButton;
 Servo gothelServo;
 const int sunButton;
 
-int pos = 0;    // variable to store the servo position
-int increment = 1;  // direction of movement (1 or -1)
+
+
+int pos = 0;                       // variable to store the servo position
+int increment = 1;                 // direction of movement (1 or -1)
 unsigned long previousMillis = 0;  // stores last time servo was updated
-const long interval = 15;  // interval between updates (milliseconds)
+const long interval = 15;          // interval between updates (milliseconds)
 
 void setup() {
+  Serial.begin(9600);
   flowerServo.attach(2);  // attaches the servo on pin 9 to the Servo object
-  pinMode(flowerButton, INPUT)
+  pinMode(flowerButt, INPUT);
 
-  hairServo.attach(5);
-  pinMode(flynnButton, INPUT)
+  // hairServo.attach(5);
+  // pinMode(flynnButt, INPUT);
 
-  gothelServo.attach(8);
+  // gothelServo.attach(8);
 
-  pinMode(sunButton, INPUT)
+  // pinMode(sunButton, INPUT);
 }
 
 void loop() {
+  flowerButtVal = digitalRead(flowerButtPin);
+  Serial.println(flowerButtVal);
+  flowerButtState = digitalRead(flowerButtPin);  // Read directly into flowerButtState
   unsigned long currentMillis = millis();
-  
-  // Check if it's time to update the servo
+
+
   if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;  // save the last time we updated
-    
-    // Update servo position
-    flowerServo.write(pos);
-    
-    // Move to next position
-    pos += increment;
-    
-    // Reverse direction at limits
-    if (pos >= 180) {
-      pos = 180;
-      increment = -1;
-    } else if (pos <= 0) {
-      pos = 0;
-      increment = 1;
+    previousMillis = currentMillis;
+
+    if (flowerButtState != lastflowerButtState) {
+      if (flowerButtState == HIGH) {
+        Serial.println("MoonLanding affirmative");
+        flowerServo.write(45);
+        flower = true;
+      } else {
+        flowerServo.write(0);
+        flower = false;
+      }
+      lastflowerButtState = flowerButtState;  // Update inside the if block
     }
   }
-  
-  // Your code is now non-blocking!
-  // You can add other tasks here that will run continuously
 }
